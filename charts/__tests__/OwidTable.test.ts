d519616956e242954517ec60502c32be0e1004af
Breck Yunits <breck7@gmail.com>
2020-06-26 21:09:58 -1000

    test updates

--- charts/__tests__/OwidTable.test.ts
+++ charts/__tests__/OwidTable.test.ts
@@ -13,7 +13,7 @@ describe(OwidTable, () => {
         }
     ]
     const table = new OwidTable(rows)
-    it("can create a table", () => {
+    it("can create a table and detect columns", () => {
         expect(table.rows.length).toEqual(1)
         expect(Array.from(table.columnsByName.keys()).length).toEqual(5)
     })
@@ -32,21 +32,20 @@ describe("rolling averages", () => {
         {
             year: 2020,
             entityName: "United States",
+            population: 3e8,
             entityId: 1,
             entityCode: "USA"
         }
     ]
     const table = new OwidTable(rows)
-    it("can create a table", () => {
+    it("a column can be added", () => {
         expect(table.rows.length).toEqual(1)
         expect(Array.from(table.columnsByName.keys()).length).toEqual(5)
-    })
-
-    it("a column can be added", () => {
         table.addColumn(
             { slug: "populationInMillions" },
             row => row.population / 1000000
         )
         expect(table.rows[0].populationInMillions).toEqual(300)
+        expect(Array.from(table.columnsByName.keys()).length).toEqual(6)
     })
 })
